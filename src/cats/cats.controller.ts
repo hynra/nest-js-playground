43e1741f6e396fd49864c7d8ebe73665cf9ed505
hynra <hynra@yahoo.com>
2019-02-05 22:01:11 +0700

    use express res

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,30 +1,10 @@
-import { Controller, Get, Post, Body, Put, Param, Delete, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, Put, Param, Delete, Query, Res, HttpStatus } from '@nestjs/common';
 
 @Controller('cats')
 export class CatsController {
-    @Post()
-    create(@Body() createCatDto) {
-        console.log(createCatDto)
-        return 'This action adds a new cat';
-    }
 
     @Get()
-    findAll(@Query() query) {
-        return `This action returns all cats (limit: ${query.limit} items)`;
-    }
-
-    @Get(':id')
-    findOne(@Param('id') id) {
-        return `This action returns a #${id} cat`;
-    }
-
-    @Put(':id')
-    update(@Param('id') id, @Body() updateCatDto) {
-        return `This action updates a #${id} cat`;
-    }
-
-    @Delete(':id')
-    remove(@Param('id') id) {
-        return `This action removes a #${id} cat`;
+    findAll(@Res() res) {
+        res.status(HttpStatus.OK).json([]);
     }
 }
