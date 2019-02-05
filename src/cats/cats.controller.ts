e8c5b105d4113c73a70e5db89e62c7825034faba
hynra <hynra@yahoo.com>
2019-02-05 20:47:07 +0700

    Resources and willcard routes

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,9 +1,21 @@
-import { Controller, Get, Req } from '@nestjs/common';
+import { Controller, Get, Req, Param, Post } from '@nestjs/common';
+import { create } from 'domain';
 
 @Controller('cats')
 export class CatsController {
+
     @Get()
     findAll() {
         return 'This action returns all cats';
+    }
+
+    @Post()
+    create() {
+        return 'This action adds a new cat'
+    }
+
+    @Get('ab*cd')
+    findAllWillcard() {
+        return 'This route uses a wildcard';
     }
 }
