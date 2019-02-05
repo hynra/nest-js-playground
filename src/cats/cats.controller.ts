2b855268ab633806764e2f54e4080636724f8fc0
hynra <hynra@yahoo.com>
2019-02-05 21:02:47 +0700

    parameter & queryparams

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,21 +1,35 @@
-import { Controller, Get, Req, Param, Post } from '@nestjs/common';
+import { Controller, Get, Req, Param, Post, Query } from '@nestjs/common';
 import { create } from 'domain';
 
 @Controller('cats')
 export class CatsController {
 
+    // standard
+    /*@Get(':id')
+    findOne(@Param() params) {
+        console.log(params.id);
+        return `This action returns a #${params.id} cat`;
+    }*/
+
+    //spesific
+    @Get(':id')
+    findOne(@Param('id') id) {
+        return `This action returns a #${id} cat`;
+    }
+
+    /*
+
+        use @Query() to fetch all queryparams
+        use @Query('name_of_query') to fetch spesific queryparam
+
+    */
+   
     @Get()
-    findAll() {
-        return 'This action returns all cats';
-    }
-
-    @Post()
-    create() {
-        return 'This action adds a new cat'
-    }
-
-    @Get('ab*cd')
-    findAllWillcard() {
-        return 'This route uses a wildcard';
+    find(@Query('test') q) {
+        console.log(q)
+        if (q)
+            return `This action returns a ${q} cat`;
+        else
+            return `This action returns not a test cat`;
     }
 }
