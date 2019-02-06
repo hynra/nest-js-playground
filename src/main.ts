e8bc2ee99cf0e73dc2600c5ec2eca8cdf957dbec
hynra <hynra@yahoo.com>
2019-02-06 15:00:46 +0700

    graphql

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { ValidationPipe } from './validation.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApplicationModule);
-  //app.useGlobalPipes(new ValidationPipe())
   await app.listen(3000);
 }
 bootstrap();
